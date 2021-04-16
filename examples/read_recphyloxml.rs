e62ca39a8327e2d304cb76dd79863732757ad98e
Simon Penel <simon.penel@univ-lyon1.fr>
2021-04-16 11:15:46 +0200

    Add read_recphyloxml example

---
+++ examples/read_recphyloxml.rs
@@ -0,0 +1,28 @@
+use light_phylogeny::{ArenaTree,Options,Config,read_recphyloxml,recphyloxml_processing};
+
+fn main() {
+
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    let config: Config = Config::new();
+    // Version portrait
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    read_recphyloxml("examples/FAM000715_reconciliated_2genes.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "read_recphyloxml_portrait.svg".to_string());
+    println!("Please open output file 'read_recphyloxml_portrait.svg' with your browser");
+
+    // Version paysage
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    read_recphyloxml("examples/FAM000715_reconciliated_2genes.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees);
+    options.rotate = false;
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "read_recphyloxml_landscape.svg".to_string());
+    println!("Please open output file 'read_recphyloxml_landscape.svg' with your browser");
+
+
+}
