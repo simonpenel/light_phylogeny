0f96d3c1a48a12fef65c6f7a304d9bbb09126761
Simon Penel <simon.penel@univ-lyon1.fr>
2021-04-16 11:15:12 +0200

    Update read_newick example

--- examples/read_newick.rs
+++ examples/read_newick.rs
@@ -5,14 +5,15 @@ fn main() {
     let options: Options = Options::new();
     let config: Config = Config::new();
     read_newick("examples/newick.txt".to_string(), &mut tree);
-    phyloxml_processing(&mut tree, &options, &config,"read_newick-clado.svg".to_string());
-    println!("Please open output file 'read_newick-clado.svg' with your browser");
+    phyloxml_processing(&mut tree, &options, &config,"read_newick_clado.svg".to_string());
+    println!("Please open output file 'read_newick_clado.svg' with your browser");
 
     let mut tree: ArenaTree<String> = ArenaTree::default();
     let mut options: Options = Options::new();
     options.real_length_flag = true;
+    options.rotate = false;
     let config: Config = Config::new();
     read_newick("examples/newick.txt".to_string(), &mut tree);
-    phyloxml_processing(&mut tree, &options, &config,"read_newick-real-clado.svg".to_string());
-    println!("Please open output file 'read_newick-real-clado.svg' with your browser");
+    phyloxml_processing(&mut tree, &options, &config,"read_newick_real_landcsape.svg".to_string());
+    println!("Please open output file 'read_newick_real_landcsape.svg' with your browser");
 }
