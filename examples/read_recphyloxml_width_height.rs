1af87a15cdc5d5989c66a8f7f8bec6cf1abaa242
Simon Penel <simon.penel@univ-lyon1.fr>
2021-05-06 14:13:11 +0200

    Add example read_recphyloxml_width_height.rs

---
+++ examples/read_recphyloxml_width_height.rs
@@ -0,0 +1,72 @@
+// Display a reconciled tree form recPhyloXML format
+
+use light_phylogeny::{ArenaTree,Options,Config,read_recphyloxml,recphyloxml_processing,
+    phyloxml_processing};
+
+fn main() {
+
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    let config: Config = Config::new();
+    // Version de base
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    read_recphyloxml("examples/FAM000715_reconciliated_2genes.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "read_recphyloxml.svg".to_string());
+    println!("Please open output file 'read_recphyloxml.svg' with your browser");
+
+    // Version height
+    options.height = 3.0;
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    read_recphyloxml("examples/FAM000715_reconciliated_2genes.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "read_recphyloxml_height.svg".to_string());
+    println!("Please open output file 'read_recphyloxml_height.svg' with your browser");
+
+    // Version width
+    options.height = 1.0;
+    options.width = 3.0;
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    read_recphyloxml("examples/FAM000715_reconciliated_2genes.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "read_recphyloxml_width.svg".to_string());
+    println!("Please open output file 'read_recphyloxml_width.svg' with your browser");
+
+    // Version width phyloxml
+    options.width = 1.0;
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    read_recphyloxml("examples/FAM000715_reconciliated_2genes.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees);
+    phyloxml_processing(&mut gene_trees[0], &mut options, &config,
+         "read_phyloxml.svg".to_string());
+    println!("Please open output file 'read_phyloxml.svg' with your browser");
+
+    // Version width phyloxml and height
+    options.height = 3.0;
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    read_recphyloxml("examples/FAM000715_reconciliated_2genes.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees);
+    phyloxml_processing(&mut gene_trees[0], &mut options, &config,
+         "read_phyloxml_height.svg".to_string());
+    println!("Please open output file 'read_phyloxml_height.svg' with your browser");
+
+    // Version width phyloxml and with
+    options.height = 1.0;
+    options.width = 3.0;
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    read_recphyloxml("examples/FAM000715_reconciliated_2genes.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees);
+    phyloxml_processing(&mut gene_trees[0], &mut options, &config,
+         "read_phyloxml_width.svg".to_string());
+    println!("Please open output file 'read_phyloxml_width.svg' with your browser");
+
+}
