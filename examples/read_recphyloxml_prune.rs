02e8186286808fd666643dfe6f7355150784fcb9
Simon Penel <simon.penel@univ-lyon1.fr>
2021-05-28 16:00:23 +0200

    Modify example read_recphyloxml_prune.rs

--- examples/read_recphyloxml_prune.rs
+++ examples/read_recphyloxml_prune.rs
@@ -6,8 +6,9 @@ fn main() {
 
     let transfers = vec![];
     let mut options: Options = Options::new();
-    let mut config: Config = Config::new();
+    let config: Config = Config::new();
     // Version portrait
+    options.species_internal = true;
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
     let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
     read_recphyloxml("examples/gene_parasite_page4.recphylo".to_string(),
@@ -16,18 +17,7 @@ fn main() {
          &transfers, "read_recphyloxml_portrait.svg".to_string());
     println!("Please open output file 'read_recphyloxml_portrait.svg' with your browser");
 
-    // Version paysage
-    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
-    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
-    read_recphyloxml("examples/gene_parasite_page4.recphylo".to_string(),
-        &mut sp_tree, &mut gene_trees);
-    options.rotate = false;
-    options.remove = true;
-    config.species_opacity = "0.3".to_string();
-    config.gene_opacity = "0.6".to_string();
-    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_landscape.svg".to_string());
-    println!("Please open output file 'read_recphyloxml_landscape.svg' with your browser");
+
 
 
 }
