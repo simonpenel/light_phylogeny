1e61d36f32c010ab173050e819c35d43543212b0
Simon Penel <simon.penel@univ-lyon1.fr>
2021-06-21 12:00:31 +0200

    Bidouille  examples/read_recphyloxml_multi.rs

--- examples/read_recphyloxml_multi.rs
+++ examples/read_recphyloxml_multi.rs
@@ -31,99 +31,11 @@ fn main() {
     // Version recphylo
     recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
          &transfers, "read_recphyloxml_multi_2.svg".to_string());
-<<<<<<< HEAD
-     // Version phylo
-=======
-         // Version phylo
->>>>>>> master
+         
     reset_pos(&mut sp_tree);
     phyloxml_processing(&mut sp_tree, &mut options, &config,
         "read_phyloxml_multi_2.svg".to_string());
 
-<<<<<<< HEAD
-=======
-
-    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
-    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
-    let mut global_roots: std::vec::Vec<usize> = Vec::new();
-    read_recphyloxml_multi("examples/ex3comp.recphyloxml".to_string(),
-        &mut sp_tree, &mut gene_trees, &mut global_roots);
-    // Version recphylo
-    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_multi_3.svg".to_string());
-         // Version phylo
-    reset_pos(&mut sp_tree);
-    phyloxml_processing(&mut sp_tree, &mut options, &config,
-        "read_phyloxml_multi_3.svg".to_string());
-
-
-    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
-    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
-    let mut global_roots: std::vec::Vec<usize> = Vec::new();
-    read_recphyloxml_multi("examples/ex4comp.recphyloxml".to_string(),
-        &mut sp_tree, &mut gene_trees, &mut global_roots);
-    // Version recphylo
-    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_multi_4.svg".to_string());
-         // Version phylo
-    reset_pos(&mut sp_tree);
-    phyloxml_processing(&mut sp_tree, &mut options, &config,
-        "read_phyloxml_multi_4.svg".to_string());
-
-    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
-    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
-    let mut global_roots: std::vec::Vec<usize> = Vec::new();
-    read_recphyloxml_multi("examples/ex5comp.recphyloxml".to_string(),
-        &mut sp_tree, &mut gene_trees, &mut global_roots);
-    // Version recphylo
-    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_multi_5.svg".to_string());
-         // Version phylo
-    reset_pos(&mut sp_tree);
-    phyloxml_processing(&mut sp_tree, &mut options, &config,
-        "read_phyloxml_multi_5.svg".to_string());
-
-    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
-    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
-    let mut global_roots: std::vec::Vec<usize> = Vec::new();
-    read_recphyloxml_multi("examples/ex6comp.recphyloxml".to_string(),
-        &mut sp_tree, &mut gene_trees, &mut global_roots);
-    // Version recphylo
-    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_multi_6.svg".to_string());
-         // Version phylo
-    reset_pos(&mut sp_tree);
-    phyloxml_processing(&mut sp_tree, &mut options, &config,
-        "read_phyloxml_multi_6.svg".to_string());
-
-
-    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
-    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
-    let mut global_roots: std::vec::Vec<usize> = Vec::new();
-    read_recphyloxml_multi("examples/ex7comp.recphyloxml".to_string(),
-        &mut sp_tree, &mut gene_trees, &mut global_roots);
-    // Version recphylo
-    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_multi_7.svg".to_string());
-         // Version phylo
-    reset_pos(&mut sp_tree);
-    phyloxml_processing(&mut sp_tree, &mut options, &config,
-        "read_phyloxml_multi_7.svg".to_string());
-
-    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
-    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
-    let mut global_roots: std::vec::Vec<usize> = Vec::new();
-    read_recphyloxml_multi("examples/ex8comp.recphyloxml".to_string(),
-        &mut sp_tree, &mut gene_trees, &mut global_roots);
-    // Version recphylo
-    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_multi_8.svg".to_string());
-         // Version phylo
-    reset_pos(&mut sp_tree);
-    phyloxml_processing(&mut sp_tree, &mut options, &config,
-        "read_phyloxml_multi_8.svg".to_string());
-
->>>>>>> master
     println!("Please open output files 'read_recphyloxml_multi.svg' with your browser");
     println!("Please open output files 'read_phyloxml_multi.svg' with your browser");
     println!("Please open output files 'read_recphyloxml_multi_2.svg' with your browser");
