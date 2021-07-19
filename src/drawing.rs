bce32954f612224de79d0a4b1b557fd4b16b90c5
Simon Penel <simon.penel@univ-lyon1.fr>
2021-07-19 14:25:51 +0200

    The link to  free living species is a bezier, and free living tree has an opacity equal to half the gene opaciy

--- src/drawing.rs
+++ src/drawing.rs
@@ -378,10 +378,8 @@ pub fn draw_sptree_gntrees (
                                     true => "0.0".to_string(),
                                     false => config.gene_opacity.to_string(),
                             };
-
                             // Verifie que le parent est bien un branchingout
                             match n.e {
-
                                 Event::BranchingOut => get_chemin_transfer(index.x,index.y,
                                         n.x,n.y,
                                         gene_color.to_string(),
@@ -406,8 +404,22 @@ pub fn draw_sptree_gntrees (
                                     config.gene_opacity.to_string(),false)
                                 }
                             else {
+                                let free_opacity =
+                                    config.gene_opacity.parse::<f32>().unwrap() / 2.0 ;
+                                if n.location == "FREE_LIVING".to_string()
+                                {
                                 get_chemin_carre(index.x,index.y,n.x,n.y ,gene_color.to_string(),
-                                "0.2".to_string(),false)
+                                free_opacity.to_string(),false)
+                                }
+                                else
+                                {
+                                    get_chemin_transfer(index.x,index.y,
+                                            n.x,n.y,
+                                            gene_color.to_string(),
+                                            free_opacity.to_string(),
+                                            config.bezier.to_string().parse::<f32>().unwrap(),
+                                            0)
+                                }
 
                             }
                                     },
