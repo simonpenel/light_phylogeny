fbc105538116295e49034f1d28a671f50274397a
Simon Penel <simon.penel@univ-lyon1.fr>
2021-07-22 15:42:32 +0200

    Fixing bug when free_living start at the root

--- examples/read_recphyloxml_free_living_2.rs
+++ examples/read_recphyloxml_free_living_2.rs
@@ -16,10 +16,10 @@ fn main() {
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
     let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
     let mut global_roots: std::vec::Vec<usize> = Vec::new();
-    read_recphyloxml_multi("examples/free_living_reconciliated.recphylo".to_string(),
+    read_recphyloxml_multi("examples/free_living_reconciliated_2.recphylo".to_string(),
         &mut sp_tree, &mut gene_trees, &mut global_roots);
     recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_free_living_reconciliated.svg".to_string());
-    println!("Please open output file 'read_recphyloxml_free_living_reconciliated.svg' with your browser");
+         &transfers, "read_recphyloxml_free_living_reconciliated_2.svg".to_string());
+    println!("Please open output file 'read_recphyloxml_free_living_reconciliated_2.svg' with your browser");
 
 }
