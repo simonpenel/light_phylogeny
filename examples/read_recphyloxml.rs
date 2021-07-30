1717ecd7c42b3593c65bd8138d6015645a626b23
Simon Penel <simon.penel@univ-lyon1.fr>
2021-07-30 14:59:02 +0200

    Comment the example which give an error

--- examples/read_recphyloxml.rs
+++ examples/read_recphyloxml.rs
@@ -175,18 +175,18 @@ fn main() {
 
     // ============================================================================================
     // Version pour  real_branch avec branche nulles (-> erreur)
-    let transfers = vec![];
-    let mut options: Options = Options::new();
-    options.real_length_flag = true;
-    let config: Config = Config::new();
-    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
-    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
-    let mut global_roots: std::vec::Vec<usize> = Vec::new();
-    read_recphyloxml_multi("examples/FAM000297_reconciliated.recphylo".to_string(),
-        &mut sp_tree, &mut gene_trees, &mut global_roots);
-    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_FAM000297_reconciliated_real.svg".to_string());
-    println!("Please open output file 'read_recphyloxml_FAM000297_reconciliated_real.svg' with your browser");
+    // let transfers = vec![];
+    // let mut options: Options = Options::new();
+    // options.real_length_flag = true;
+    // let config: Config = Config::new();
+    // let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    // let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    // let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    // read_recphyloxml_multi("examples/FAM000297_reconciliated.recphylo".to_string(),
+    //     &mut sp_tree, &mut gene_trees, &mut global_roots);
+    // recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+    //      &transfers, "read_recphyloxml_FAM000297_reconciliated_real.svg".to_string());
+    // println!("Please open output file 'read_recphyloxml_FAM000297_reconciliated_real.svg' with your browser");
 
 
 
