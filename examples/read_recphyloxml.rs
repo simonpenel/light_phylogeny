f84f328f3ad204ed8e277641efd9eba7799fb9b5
Simon Penel <simon.penel@univ-lyon1.fr>
2021-07-30 14:50:39 +0200

    Adding examples for pipe tree real length handling

--- examples/read_recphyloxml.rs
+++ examples/read_recphyloxml.rs
@@ -4,10 +4,11 @@ use light_phylogeny::{ArenaTree,Options,Config,read_recphyloxml_multi,recphyloxm
 
 fn main() {
 
+    // ============================================================================================
+    // Version portrait
     let transfers = vec![];
     let mut options: Options = Options::new();
-    let mut config: Config = Config::new();
-    // Version portrait
+    let config: Config = Config::new();
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
     let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
     let mut global_roots: std::vec::Vec<usize> = Vec::new();
@@ -17,7 +18,11 @@ fn main() {
          &transfers, "read_recphyloxml_portrait.svg".to_string());
     println!("Please open output file 'read_recphyloxml_portrait.svg' with your browser");
 
+    // ============================================================================================
     // Version paysage
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    let mut config: Config = Config::new();
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
     let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
     let mut global_roots: std::vec::Vec<usize> = Vec::new();
@@ -30,11 +35,11 @@ fn main() {
          &transfers, "read_recphyloxml_landscape.svg".to_string());
     println!("Please open output file 'read_recphyloxml_landscape.svg' with your browser");
 
+    // ============================================================================================
     // Version multiple
+    let transfers = vec![];
     let mut options: Options = Options::new();
     let config: Config = Config::new();
-
-
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
     let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
     let mut global_roots: std::vec::Vec<usize> = Vec::new();
@@ -44,12 +49,12 @@ fn main() {
          &transfers, "read_recphyloxml_mult.svg".to_string());
     println!("Please open output file 'read_recphyloxml_mult.svg' with your browser");
 
-
+    // ============================================================================================
     // Version multiple uniforme
+    let transfers = vec![];
     let mut options: Options = Options::new();
     let config: Config = Config::new();
     options.uniform = true;
-
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
     let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
     let mut global_roots: std::vec::Vec<usize> = Vec::new();
@@ -59,14 +64,11 @@ fn main() {
          &transfers, "read_recphyloxml_mult_uni.svg".to_string());
     println!("Please open output file 'read_recphyloxml_mult_uni.svg' with your browser");
 
-
-
-
+    // ============================================================================================
     // Version pour verifier la gestion des duplication
+    let transfers = vec![];
     let mut options: Options = Options::new();
     let config: Config = Config::new();
-
-
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
     let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
     let mut global_roots: std::vec::Vec<usize> = Vec::new();
@@ -76,7 +78,8 @@ fn main() {
          &transfers, "read_recphyloxml_FAM000297_reconciliated.svg".to_string());
     println!("Please open output file 'read_recphyloxml_FAM000297_reconciliated.svg' with your browser");
 
-    // Version pour  avec loption uniform
+    // ============================================================================================
+    // Version pour verifier la gestion des duplication et option uniforme
     let mut options: Options = Options::new();
     options.uniform = true;
     let config: Config = Config::new();
@@ -89,25 +92,12 @@ fn main() {
          &transfers, "read_recphyloxml_FAM000297_reconciliated_uni.svg".to_string());
     println!("Please open output file 'read_recphyloxml_FAM000297_reconciliated_uni.svg' with your browser");
 
-    // Version pour  avec loption uniform et real_branc
 
-    let mut options: Options = Options::new();
-    options.uniform = true;
-    options.real_length_flag = true;
-    let config: Config = Config::new();
-    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
-    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
-    let mut global_roots: std::vec::Vec<usize> = Vec::new();
-    read_recphyloxml_multi("examples/FAM000297_reconciliated.recphylo".to_string(),
-        &mut sp_tree, &mut gene_trees, &mut global_roots);
-    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "read_recphyloxml_FAM000297_reconciliated_real.svg".to_string());
-    println!("Please open output file 'read_recphyloxml_FAM000297_reconciliated_real.svg' with your browser");
-
-
+    // ============================================================================================
+    // Versions avec longueur de branche
 
     // Version  simple
-
+    let transfers = vec![];
     let mut options: Options = Options::new();
     let config: Config = Config::new();
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
@@ -119,12 +109,11 @@ fn main() {
          &transfers, "hote_parasite_page4_BL.svg".to_string());
     println!("Please open output file 'hote_parasite_page4_BL.svg' with your browser");
 
-
-    // Version pour  avec loption uniform
+    // Version pour  avec l option uniform
+    let transfers = vec![];
     let mut options: Options = Options::new();
+    options.species_internal = true;
     options.uniform = true;
-    // options.real_length_flag = true;
-    // options.scale = 1.0;
     let config: Config = Config::new();
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
     let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
@@ -135,11 +124,11 @@ fn main() {
          &transfers, "hote_parasite_page4_BL_uni.svg".to_string());
     println!("Please open output file 'hote_parasite_page4_BL_uni.svg' with your browser");
 
-    // Version pour  avec loption uniform etreal length
+    // Version pour  avec l option real length
+    let transfers = vec![];
     let mut options: Options = Options::new();
-    options.uniform = true;
     options.real_length_flag = true;
-    options.scale = 8.0;
+    options.species_internal = true;
     let config: Config = Config::new();
     let mut sp_tree: ArenaTree<String> = ArenaTree::default();
     let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
@@ -149,6 +138,56 @@ fn main() {
     recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
          &transfers, "hote_parasite_page4_BL_real.svg".to_string());
     println!("Please open output file 'hote_parasite_page4_BL_real.svg' with your browser");
+
+    // ============================================================================================
+    // Versions avec longueur de branche 2
+
+    // Version pour  avec l option uniform
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    options.uniform = true;
+    options.species_internal = true;
+    let config: Config = Config::new();
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    read_recphyloxml_multi("examples/hote_parasite_page4_BL_2.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "hote_parasite_page4_BL_uni_2.svg".to_string());
+    println!("Please open output file 'hote_parasite_page4_BL_uni_2.svg' with your browser");
+
+    // Version pour  avec l option real length
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    options.real_length_flag = true;
+    options.species_internal = true;
+    let config: Config = Config::new();
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    read_recphyloxml_multi("examples/hote_parasite_page4_BL_2.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "hote_parasite_page4_BL_real_2.svg".to_string());
+    println!("Please open output file 'hote_parasite_page4_BL_real_2.svg' with your browser");
+
+
+    // ============================================================================================
+    // Version pour  real_branch avec branche nulles (-> erreur)
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    options.real_length_flag = true;
+    let config: Config = Config::new();
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    read_recphyloxml_multi("examples/FAM000297_reconciliated.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "read_recphyloxml_FAM000297_reconciliated_real.svg".to_string());
+    println!("Please open output file 'read_recphyloxml_FAM000297_reconciliated_real.svg' with your browser");
+
 
 
 }
--- examples/read_recphyloxml_big.rs
+++ examples/read_recphyloxml_big.rs
@@ -33,6 +33,31 @@ fn main() {
     println!("Please open output file 'read_recphyloxml_landscape.svg' with your browser");
 
 
+    // // Version real_branch
+    //
+    // let transfers = vec![];
+    // // Version pour  avec loption uniform etreal length
+    // let mut options: Options = Options::new();
+    // options.uniform = true;
+    // options.real_length_flag = true;
+    //
+    // let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    // let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    // let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    // read_recphyloxml_multi("examples/gene_tree_HBG011000.newick_recs.recphylo".to_string(),
+    //     &mut sp_tree, &mut gene_trees, &mut global_roots);
+    // let _pipeblock    = 15.0;
+    // let min_dist = sp_tree.get_smallest_l();
+    // // we want min_dist = _pipeblock
+    // if min_dist == 0.0 {
+    //     panic!("There is branch distance equal to 0.0");
+    // }
+    //
+    // recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+    //      &transfers, "read_recphyloxml_real.svg".to_string());
+    // println!("Please open output file 'read_recphyloxml_real.svg' with your browser");
+
+
 
 
 }
