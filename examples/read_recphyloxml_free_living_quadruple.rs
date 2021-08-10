f319662079d2bf90f7ca7bb4a490ef1968f2bb9f
Simon Penel <simon.penel@univ-lyon1.fr>
2021-08-10 10:54:56 +0200

    Adding examples

---
+++ examples/read_recphyloxml_free_living_quadruple.rs
@@ -0,0 +1,25 @@
+// Display a reconciled tree form recPhyloXML format
+
+use light_phylogeny::{ArenaTree,Options,Config,read_recphyloxml_multi,recphyloxml_processing};
+
+fn main() {
+
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    let config: Config = Config::new();
+
+    options.free_living = true;
+    options.gene_internal = true;
+    options.species_internal = true;
+
+
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    read_recphyloxml_multi("examples/free_living_reconciliated_quadruple.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "read_recphyloxml_free_living_reconciliated_quadruple.svg".to_string());
+    println!("Please open output file 'read_recphyloxml_free_living_reconciliated_quadruple.svg' with your browser");
+
+}
