dfc6b3808be68606c3f9237193b09b8b73ea39fe
Simon Penel <simon.penel@univ-lyon1.fr>
2022-03-04 15:56:59 +0100

    Adding examples for virus with tidy

--- examples/read_phyloxml_tidy_virus.rs
+++ examples/read_phyloxml_tidy_virus.rs
@@ -28,4 +28,32 @@ fn main() {
     phyloxml_processing(&mut tree, &options, &config,"read_phyloxml_dist_real_virus_tidy.svg".to_string());
     println!("Please open output file 'read_phyloxml_dist_real_virus_tidy.svg' with your browser");
 
+
+    let mut tree: ArenaTree<String> = ArenaTree::default();
+    let mut options: Options = Options::new();
+    options.real_length_flag = true;
+    options.scale = 4.0;
+    // options.rotate = false;
+    // options.support = true;
+    // options.gene_internal = true;
+    // options.branch = true;
+    let config: Config = Config::new();
+    read_newick("examples/virus.nhx".to_string(), &mut tree);
+    phyloxml_processing(&mut tree, &options, &config,"read_phyloxml_dist_real_virus_portrait.svg".to_string());
+    println!("Please open output file 'read_phyloxml_dist_real_virus_portrait.svg' with your browser");
+
+    let mut tree: ArenaTree<String> = ArenaTree::default();
+    let mut options: Options = Options::new();
+    options.real_length_flag = true;
+    options.scale = 4.0;
+    // options.rotate = false;
+    options.tidy = true;
+    // options.support = true;
+    // options.gene_internal = true;
+    // options.branch = true;
+    let config: Config = Config::new();
+    read_newick("examples/virus.nhx".to_string(), &mut tree);
+    phyloxml_processing(&mut tree, &options, &config,"read_phyloxml_dist_real_virus_portrait_tidy.svg".to_string());
+    println!("Please open output file 'read_phyloxml_dist_real_virus_portrait_tidy.svg' with your browser");
+
 }
