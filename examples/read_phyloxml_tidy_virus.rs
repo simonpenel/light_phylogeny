5b8e0183e4f880d2969ab6d1ceee52a2dcda520f
Simon Penel <simon.penel@univ-lyon1.fr>
2022-03-04 16:36:23 +0100

    Display branch length in read_phyloxml_tidy_virus example

--- examples/read_phyloxml_tidy_virus.rs
+++ examples/read_phyloxml_tidy_virus.rs
@@ -8,7 +8,7 @@ fn main() {
     options.rotate = false;
     // options.support = true;
     // options.gene_internal = true;
-    // options.branch = true;
+    options.branch = true;
     let config: Config = Config::new();
     read_newick("examples/virus.nhx".to_string(), &mut tree);
     phyloxml_processing(&mut tree, &options, &config,"read_phyloxml_dist_real_virus.svg".to_string());
@@ -22,7 +22,7 @@ fn main() {
     options.tidy = true;
     // options.support = true;
     // options.gene_internal = true;
-    // options.branch = true;
+    options.branch = true;
     let config: Config = Config::new();
     read_newick("examples/virus.nhx".to_string(), &mut tree);
     phyloxml_processing(&mut tree, &options, &config,"read_phyloxml_dist_real_virus_tidy.svg".to_string());
@@ -36,7 +36,7 @@ fn main() {
     // options.rotate = false;
     // options.support = true;
     // options.gene_internal = true;
-    // options.branch = true;
+    options.branch = true;
     let config: Config = Config::new();
     read_newick("examples/virus.nhx".to_string(), &mut tree);
     phyloxml_processing(&mut tree, &options, &config,"read_phyloxml_dist_real_virus_portrait.svg".to_string());
@@ -50,7 +50,7 @@ fn main() {
     options.tidy = true;
     // options.support = true;
     // options.gene_internal = true;
-    // options.branch = true;
+    options.branch = true;
     let config: Config = Config::new();
     read_newick("examples/virus.nhx".to_string(), &mut tree);
     phyloxml_processing(&mut tree, &options, &config,"read_phyloxml_dist_real_virus_portrait_tidy.svg".to_string());
