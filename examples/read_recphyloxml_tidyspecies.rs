69ee4196277c1289abd922d1a24c1b629da001cb
Simon Penel <simon.penel@univ-lyon1.fr>
2022-03-22 10:32:48 +0100

    Adding portrait version in read_recphyloxml_tidyspecies example

--- examples/read_recphyloxml_tidyspecies.rs
+++ examples/read_recphyloxml_tidyspecies.rs
@@ -51,7 +51,7 @@ fn main() {
     let transfers = vec![];
     let mut options: Options = Options::new();
     options.real_length_flag = true;
-    options.scale = 0.20;
+    options.scale = 1.0;
     // options.rotate = false;
     options.tidy = true;
     // options.tidy_leaves_check = true;
@@ -67,6 +67,28 @@ fn main() {
     recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
          &transfers, "FAM000297_reconciliated_BL_tidy.svg".to_string());
     println!("Please open output file 'FAM000297_reconciliated_BL_tidy.svg' with your browser");
+
+
+    // ============================================================================================
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    options.real_length_flag = true;
+    options.scale = 1.0;
+    options.rotate = false;
+    options.tidy = true;
+    // options.tidy_leaves_check = true;
+    options.species_internal = true;
+
+    let mut config: Config = Config::new();
+    config.species_police_size = 40.0.to_string();
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    read_recphyloxml_multi("examples/FAM000297_reconciliated_BL.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "FAM000297_reconciliated_BL_portrait_tidy.svg".to_string());
+    println!("Please open output file 'FAM000297_reconciliated_BL_portrait_tidy.svg' with your browser");
 
 
 
