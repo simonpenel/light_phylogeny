e2c14a916cbb0a405a3ce90d4dea60dbdec31ff8
Simon Penel <simon.penel@univ-lyon1.fr>
2022-09-05 16:19:37 +0200

    Add examples for start and end options of transfert

--- examples/read_recphyloxml_thickness.rs
+++ examples/read_recphyloxml_thickness.rs
@@ -67,4 +67,42 @@ fn main() {
     println!("Please open output file 'read_recphyloxml_threshold_3.svg' with your browser");
 
 
+    // Version opacite et seuil
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    read_recphyloxml_multi("examples/concat.xml".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    options.thickness_flag = true;
+    options.thickness_thresh = 0;
+    options.thickness_disp_score = true;
+    options.rotate = false;
+    options.trans_end=Some("species_17".to_string());
+    let mut selected_gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    selected_gene_trees.push(gene_trees.remove(options.thickness_gene));
+    recphyloxml_processing(&mut sp_tree, &mut selected_gene_trees, &mut options, &config, true,
+         &gene_transfers, "read_recphyloxml_threshold_4.svg".to_string());
+    println!("Please open output file 'read_recphyloxml_threshold_4.svg' with your browser");
+
+    // Version opacite et seuil
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    read_recphyloxml_multi("examples/concat.xml".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    options.thickness_flag = true;
+    options.thickness_thresh = 0;
+    options.thickness_disp_score = true;
+    options.rotate = false;
+    options.trans_end=Some("species_18".to_string());
+    let mut selected_gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    selected_gene_trees.push(gene_trees.remove(options.thickness_gene));
+    recphyloxml_processing(&mut sp_tree, &mut selected_gene_trees, &mut options, &config, true,
+         &gene_transfers, "read_recphyloxml_threshold_5.svg".to_string());
+    println!("Please open output file 'read_recphyloxml_threshold_5.svg' with your browser");
+
+
+
+
+
 }
