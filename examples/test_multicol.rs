8d99c84ed01869a7630d0c80ac5501da58a4a451
Simon Penel <simon.penel@univ-lyon1.fr>
2024-05-22 11:07:02 +0200

    More examples for option -q

--- examples/test_multicol.rs
+++ examples/test_multicol.rs
@@ -19,8 +19,8 @@ fn main() {
     read_recphyloxml_multi("examples/toyrec.xml".to_string(),
         &mut sp_tree, &mut gene_trees, &mut global_roots);
     recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "prout1.svg".to_string());
-    println!("Please open output file 'prout1.svg' with your browser");
+         &transfers, "toy1.svg".to_string());
+    println!("Please open output file 'toy1.svg' with your browser");
     
     
     // ============================================================================================
@@ -42,8 +42,8 @@ fn main() {
     read_recphyloxml_multi("examples/toyrec.xml".to_string(),
         &mut sp_tree, &mut gene_trees, &mut global_roots);
     recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "prout2.svg".to_string());
-    println!("Please open output file 'prout2.svg' with your browser");
+         &transfers, "toy2.svg".to_string());
+    println!("Please open output file 'toy2.svg' with your browser");
     
     
     
@@ -64,6 +64,50 @@ fn main() {
     read_recphyloxml_multi("examples/toyrec.xml".to_string(),
         &mut sp_tree, &mut gene_trees, &mut global_roots);
     recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
-         &transfers, "prout3.svg".to_string());
-    println!("Please open output file 'prout3.svg' with your browser");    
+         &transfers, "toy3.svg".to_string());
+    println!("Please open output file 'toy3.svg' with your browser");    
+
+
+   // ============================================================================================
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    let config: Config = Config::new();
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+ 
+    options.gene_colors.push("black".to_string());   
+    options.gene_colors.push("green".to_string());
+    options.gene_colors.push("blue".to_string());
+    options.gene_colors.push("orange".to_string());
+
+    options.node_colors.push("03_duplication".to_string());   
+    options.node_colors.push("b4_duplication".to_string());
+    
+    read_recphyloxml_multi("examples/example_dupli.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "dupli_color.svg".to_string());
+    println!("Please open output file 'dupli_color.svg' with your browser"); 
+
+
+   // ============================================================================================
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    let config: Config = Config::new();
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+ 
+
+    options.node_colors.push("03_duplication".to_string());   
+    options.node_colors.push("b4_duplication".to_string());
+    
+    read_recphyloxml_multi("examples/example_dupli.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "dupli_color2.svg".to_string());
+    println!("Please open output file 'dupli_color2.svg' with your browser");        
+
 }
+
