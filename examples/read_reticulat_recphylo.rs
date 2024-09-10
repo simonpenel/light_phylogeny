da22422196b461208a40a14e7c665a08fc3111a9
Simon Penel <simon.penel@univ-lyon1.fr>
2024-09-10 14:30:46 +0200

    Add some example for reticulate

---
+++ examples/read_reticulat_recphylo.rs
@@ -0,0 +1,23 @@
+// Display a reconciled tree form recPhyloXML format
+
+use light_phylogeny::{ArenaTree,Options,Config,read_recphyloxml_multi,recphyloxml_processing,phyloxml_processing};
+// use std::env;
+
+fn main() {
+
+    // ============================================================================================
+    // Version portrait
+    let transfers = vec![];
+    let mut options: Options = Options::new();
+    let config: Config = Config::new();
+    let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+    let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+    let mut global_roots: std::vec::Vec<usize> = Vec::new();
+    read_recphyloxml_multi("examples/reti.recphylo".to_string(),
+        &mut sp_tree, &mut gene_trees, &mut global_roots);
+    recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+         &transfers, "example_reti.svg".to_string());
+    println!("Please open output file 'example_reti.svg' with your browser");
+
+
+}
