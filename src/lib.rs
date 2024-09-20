9e68767c64a453955e2641aa8aeac5c515977e5b
Simon Penel <simon.penel@univ-lyon1.fr>
2024-09-20 11:18:10 +0200

    Improve tests

--- src/lib.rs
+++ src/lib.rs
@@ -165,21 +165,25 @@ mod tests {
     }
     #[test]
     fn  check_read_recphylo() {
-        let mut tree: ArenaTree<String> = ArenaTree::default();
-        let options: Options = Options::new();
+        let mut sp_tree: ArenaTree<String> = ArenaTree::default();
+        let mut gene_trees:std::vec::Vec<ArenaTree<String>> = Vec::new();
+        let mut global_roots: std::vec::Vec<usize> = Vec::new();
+        let transfers = vec![];
         let config: Config = Config::new();
-        let _root = tree.new_node("Root".to_string());
-        phyloxml_processing(&mut tree, &options, &config, "toto".to_string() ) ;
-        println!("Tree {:?}",tree);
+        let mut options: Options = Options::new();
+        read_recphyloxml_multi("recphylo_examples/FAM000297_reconciliated.recphylo".to_string(),
+            &mut sp_tree, &mut gene_trees, &mut global_roots);
+        recphyloxml_processing(&mut sp_tree, &mut gene_trees, &mut options, &config, true,
+                 &transfers, "toto.svg".to_string());
+        println!("Tree {:?}",sp_tree);
     }
     #[test]
     fn  check_read_phylo() {
         let mut tree: ArenaTree<String> = ArenaTree::default();
         let options: Options = Options::new();
         let config: Config = Config::new();
-        let _root = tree.new_node("Root".to_string());
-        phyloxml_processing(&mut tree, &options, &config, "toto".to_string() ) ;
-        println!("Tree {:?}",tree);
+        read_phyloxml("examples/FAM036542_gene.xml".to_string(), &mut tree);
+        phyloxml_processing(&mut tree, &options, &config,"read_phyloxml.svg".to_string());
     }
     #[test]
     fn  check_get_index() {
